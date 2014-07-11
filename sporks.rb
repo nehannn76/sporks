4ffda609005d5f65a031ba5b431fbb9a8da45dbb
Jeremy R. Budnack <jrbudnack@gmail.com>
2014-07-11 02:02:03 -0400

    Added the Customer List test data plus page rendering.

--- sporks.rb
+++ sporks.rb
@@ -1,10 +1,12 @@
 require 'sinatra'
 require 'haml'
+require 'json'
 
 get '/' do
   haml :index, :format => :html5
 end
 
 get '/customers' do
-  haml :customers, :format => :html5
+  data = JSON.parse(File.read('testdata.json'))
+  haml :customers, :format => :html5, :locals => {:customers => data}
 end
